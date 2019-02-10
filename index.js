6f3dab09f20ac9531f89d99a07181f253e90b979
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-02-10 21:16:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue > 2500) {
+    result = "No can do.";
+  }
+  else if (someValue >= 2000) {
+    result = "I will gladly take your thirty bucks.";
+    }
+  else if (someValue <= 400){
+    result = "This one is on me!";
+  }
+  return result
+}
+
+function ternaryCheckCity(someCity) {
+
+  let result;
+
+return (someCity === "NYC") ? result = "Ok, sounds good." : result = "No go.";
+}
+
+function switchOnCharmFromTip(someTip) {
+  let response;
+  switch (someTip) {
+    case "generous":
+    response = "Thank you so much.";
+      break;
+    case "not as generous":
+    response = "Thank you.";
+      break;
+    default:
+    response = "Bye.";
+  }
+  return response
+}
